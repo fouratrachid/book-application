b0f8246bf25fc59c327b59e54b2cf635798b6d39
foufou <rachidfourat2@gmail.com>
2024-05-09 20:27:53 +0100

    Initial commit

---
+++ book-social-network/src/main/java/com/fourat/book/BookSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.fourat.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ book-social-network/src/test/java/com/fourat/book/BookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourat.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
